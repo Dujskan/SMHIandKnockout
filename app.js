005cc50198c925c39dc76c0891d0f47d6a6b4ad5
Dujskan <sebbelundin@hotmail.com>
2016-02-29 19:24:49 +0100

    added some visuals

--- app.js
+++ app.js
@@ -1,29 +1,71 @@
+function WeatherViewModel(){
+    var self = this;
+
+    self.locationData = ko.observableArray();
+    self.locationName = ko.observable('');
+
+    //West and East
+    self.long = ko.observable('16');
+
+    //South and North
+    self.lat = ko.observable('56.3586');
 
 
-/*
-$.ajax("https://api.themoviedb.org/3/movie/525?api_key=34a0e4bc683651c4f090a51086e8e0fc",{
-    method: "get",
-    success: function(response){
-        response.genres.forEach(function(item){
-            document.getElementById("genres").innerHTML += '<li>' + item.name + '</li>';
-        });
+    self.longAndLatIsInRange = function(){
+        var long = parseFloat(self.long());
+        var lat = parseFloat(self.lat());
 
-return;
-        for(var i = 0; i < response.genres.length;i++){
+        return (self.locationName().length > 0) &&
+            (lat <= 70.75 && lat >= 52.50) &&
+            (long >= 2.25 && long <= 38.00);
+    };
 
-            var liELement = document.createElement('li');
-            var textElement = document.createTextNode(response.genres[i].name)
-            liELement.appendChild(textElement);
-            document.getElementById("genres").appendChild(liELement);
-        }
+    self.deleteLoaction = function(location){
+        self.locationData.remove(location)
     }
 
-});
+    self.getWeatherData = function(){
 
+        var url = 'http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/' + self.lat() + '/lon/' + self.long() + '/data.json';
 
-/*
-POST = SKAPA        C
-GET = hämta data    R
-PUT= UPDATE         U
-DELETE = delete     D
-*/
+        $.ajax({
+            url: url,
+            type: 'GET',
+            crossDomain: true,
+            success: function (response) {
+
+                var average;
+                var degrees = response.timeseries.map(function(item){return item.tcc;}).sort()
+                var median = degrees[(Math.round(degrees.length / 2))];
+
+                var sum = 0;
+                degrees.forEach(function(degree){
+
+                    sum += degree;
+                })
+                average = sum / degrees.length;
+
+                self.locationData.push({
+                    locationName : self.locationName(),
+                    long: self.long(),
+                    lat: self.lat(),
+                    average: average,
+                    mean: median});
+
+                self.locationName('');
+                self.long('');
+                self.lat('');
+
+            },
+            error: function (response) {
+                if(response.state() == 'rejected'){
+                    alert('FIELD POINT OUT OF BOUNDS')
+                }
+            }
+        });
+    }
+
+}
+
+var viewModel = new WeatherViewModel()
+ko.applyBindings(viewModel);
